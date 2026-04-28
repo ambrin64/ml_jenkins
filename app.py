7c6844b2683374fa114bf5979edc4155e4b08d39
Ambrin <ambrin263@gmail.com>
2026-04-28 12:21:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = load_iris()
+X = data.data
+y = data.target
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Predict
+pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, pred))
